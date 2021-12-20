138e57139358f35e042e50269a920d16e17c8c78
eeju <eejumoon@gmail.com>
2021-12-20 20:22:09 +0900

    first page composition

--- OddEvenGame/OddEvenGame/ViewController.swift
+++ OddEvenGame/OddEvenGame/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var computerBallCountLbl: UILabel!
+    @IBOutlet weak var userBallCountLbl: UILabel!
+    
+    var comBallCount: Int = 20
+    var userBallCount: Int = 20
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        computerBallCountLbl.text = String(comBallCount)
+        userBallCountLbl.text = String(userBallCount)
     }
 
 
+    @IBAction func gameStartPressed(_ sender: Any) {
+        print("game start!")
+    }
 }
 
